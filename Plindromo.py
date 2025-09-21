d62e0a81dd15e1de46d576da9869a8e38c071faf
Salvador-GA617 <157680296+Salvador-GA617@users.noreply.github.com>
2025-09-21 14:01:38 -0600

    Add files via upload

---
+++ Plindromo.py
@@ -0,0 +1,9 @@
+def es_palindromo(cadena):
+    if len(cadena) <= 1:
+        return True
+    if cadena[0] != cadena[-1]:
+        return False
+    return es_palindromo(cadena[1:-1])
+
+texto = "anitalavalatina"
+print("¿Es un palíndromo?", es_palindromo(texto))  
